9675248c2cd8c550818fa2f80faf536dcea109b7
NattanFerreira <nattanferreiralopes@gmail.com>
2024-03-13 15:06:38 -0300

    edit main

--- TAD_geral/settings.c
+++ TAD_geral/settings.c
@@ -1,7 +1,14 @@
 #include "settings.h"
 
 void menu(void){
-
+    printf("***************************\n");
+    printf("\t\tCadastro de móveis/setores.\n");
+    printf("1-\n");
+    printf("2-\n");
+    printf("3-\n");
+    printf("4-\n");
+    printf("5-\n");
+    printf("***************************\n");    
 }
 
 void limpa_buffer(void){
--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include "settings.h"
 
 int main(void){
+    char opcao;
+    
+    do
+    {
+        menu();
+        printf("\nDigite uma opcão:");
+        scanf("%c", &opcao);
+        
+    } while (opcao != 5);
 
+
+    if (opcao >=49 && opcao < 54){ //tabela ascii de 1 a 5
+        
+    }
+    
     return 0;
 }
