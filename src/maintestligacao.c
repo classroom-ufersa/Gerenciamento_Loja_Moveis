3043de010bd67c8d89d919fa717f211c5fda1fb3
Nattan Ferreira Lopes <nattanferreiralopes@gmail.com>
2024-04-01 14:22:24 -0300

    acentos

--- src/maintestligacao.c
+++ src/maintestligacao.c
@@ -8,7 +8,7 @@ int main() {
     lista_setores = lista_setor_adiciona_ordenado(lista_setores, "Quarto", "Setor de moveis para quarto");
 
     lista_setores->moveis = lista_movel_adiciona_ordenado(lista_setores->moveis, "Mesa", "Movel", 150.0, 5);
-    lista_setores->moveis = lista_movel_adiciona_ordenado(lista_setores->moveis, "Cadeira", "Móvel", 50.0, 10);
+    lista_setores->moveis = lista_movel_adiciona_ordenado(lista_setores->moveis, "Cadeira", "Movel", 50.0, 10);
     lista_setores->proximo->moveis = lista_movel_adiciona_ordenado(lista_setores->proximo->moveis, "Sofa", "Movel", 300.0, 3);
     lista_setores->proximo->moveis = lista_movel_adiciona_ordenado(lista_setores->proximo->moveis, "Poltrona", "Movel", 200.0, 7);
     lista_setores->proximo->proximo->moveis = lista_movel_adiciona_ordenado(lista_setores->proximo->proximo->moveis, "Cama", "Movel", 250.0, 2);
