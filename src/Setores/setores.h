df0f204d12c16ab6b806c53e2fd5539c7e184990
marcelohenrique0 <marcelo123.mh91@gmail.com>
2024-04-12 01:37:09 -0300

    doxygen header setores.h

--- src/Setores/setores.h
+++ src/Setores/setores.h
@@ -1,38 +1,98 @@
+/**
+ * @file setor.h
+ * @brief Este arquivo contém definições de estruturas e funções relacionadas aos setores.
+ */
+
 #ifndef SETOR_H
 #define SETOR_H
 
 #include "../Moveis/moveis.c"
 
+/**
+ * @struct setor
+ * @brief Estrutura que representa um setor.
+ */
 typedef struct setor Setor;
 
-// Cria uma lista de setores
+/**
+ * @brief Cria uma lista de setores.
+ * 
+ * @return Um ponteiro para a lista de setores criada.
+ */
 Setor* lista_setor_cria(void);
 
-// Adiciona um setor na lista de forma ordenada
+/**
+ * @brief Adiciona um setor na lista de forma ordenada.
+ * 
+ * @param Lista A lista de setores onde o setor será adicionado.
+ * @param nome O nome do setor a ser adicionado.
+ * @param descricao A descrição do setor a ser adicionado.
+ * @return Um ponteiro para a lista atualizada de setores.
+ */
 Setor* lista_setor_adiciona_ordenado(Setor* Lista, char nome[], char descricao[]);
 
-// Busca um setor na lista
+/**
+ * @brief Busca um setor na lista pelo nome.
+ * 
+ * @param nome O nome do setor a ser buscado.
+ * @param lista A lista de setores onde o setor será buscado.
+ * @return Um ponteiro para o setor encontrado, ou NULL se não encontrado.
+ */
 Setor* lista_setor_busca(char nome[], Setor* lista);
 
-// Remove um setor da lista
-Setor* lista_setor_remove(Setor*lista, char nome[]);
+/**
+ * @brief Remove um setor da lista pelo nome.
+ * 
+ * @param lista A lista de setores onde o setor será removido.
+ * @param nome O nome do setor a ser removido.
+ * @return Um ponteiro para a lista atualizada de setores.
+ */
+Setor* lista_setor_remove(Setor* lista, char nome[]);
 
-// Le um arquivo e salva os setores e moveis
-Setor *lista_ler_no_arquivo(Setor* lista, char nome[]);
+/**
+ * @brief Lê um arquivo e salva os setores e móveis.
+ * 
+ * @param lista A lista de setores onde os setores e móveis serão salvos.
+ * @param nome O nome do arquivo a ser lido.
+ * @return Um ponteiro para a lista atualizada de setores.
+ */
+Setor* lista_ler_no_arquivo(Setor* lista, char nome[]);
 
-// Imprime os moveis de um setor
+/**
+ * @brief Imprime os móveis de um setor.
+ * 
+ * @param lista A lista de setores onde os móveis serão impressos.
+ */
 void imprime_moveis_setor(Setor* lista);
 
-// Libera lista lista de setores
+/**
+ * @brief Libera a memória alocada para a lista de setores.
+ * 
+ * @param lista A lista de setores a ser liberada.
+ */
 void lista_setor_libera(Setor* lista);
 
-// Imprime lista lista de setores
+/**
+ * @brief Imprime a lista de setores.
+ * 
+ * @param lista A lista de setores a ser impressa.
+ */
 void lista_setor_imprime(Setor* lista);
 
-// Salva os setores e moveis em um arquivo
+/**
+ * @brief Salva os setores e móveis em um arquivo.
+ * 
+ * @param lista A lista de setores a ser salva.
+ * @param nome O nome do arquivo onde os setores e móveis serão salvos.
+ */
 void lista_escrever_no_arquivo(Setor* lista, char nome[]);
 
-// Verifica se lista lista de setores esta vazia
+/**
+ * @brief Verifica se a lista de setores está vazia.
+ * 
+ * @param lista A lista de setores a ser verificada.
+ * @return 1 se a lista estiver vazia, 0 caso contrário.
+ */
 int lista_setor_vazia(Setor* lista);
 
-#endif //SETOR_H
+#endif // SETOR_H
