7e81be0dfe805a541c1c699696b379f2b83427c6
Nattan Ferreira Lopes <nattanferreiralopes@gmail.com>
2024-04-15 11:32:17 -0300

    refact: reestruturação

---
+++ project/src/moveis.c
@@ -0,0 +1,111 @@
+#include "../includes/moveis.h"
+
+struct movel { 
+    char nome[Max];
+    char tipo[Max];
+    float preco;
+    int qtd_estoque;
+    struct movel* proximo;
+};
+
+Movel* lista_movel_cria(void) {
+    return NULL;
+}
+
+Movel* lista_movel_adiciona_ordenado(Movel* Lista, char nome[], char tipo[], float preco, int qtd_estoque){
+    Movel* novo;
+	Movel* anterior = NULL;
+	Movel* p = Lista;
+	while(p!=NULL && strcmp(nome, p->nome) > 0){
+		anterior = p;
+		p = p->proximo;
+	}
+	novo = (Movel*) malloc(sizeof(Movel));
+	if (novo == NULL){
+		printf("Erro de alocacao na lista_movel_adiciona_ordenado\n");
+		exit(1);
+	}
+	strcpy(novo->nome,nome);
+    strcpy(novo->tipo,tipo);
+	novo->preco = preco;
+	novo->qtd_estoque = qtd_estoque;
+	if(anterior == NULL){
+		novo->proximo = Lista;
+		Lista = novo;
+	}
+	else {
+		novo->proximo = anterior-> proximo;
+		anterior->proximo = novo;
+	}
+	printf("Movel adicionado com sucesso\n");
+	return Lista;
+}
+
+Movel *lista_movel_busca(Movel* lista, char nome[]){
+	Movel *p = lista;
+	for(p; p!=NULL; p=p->proximo){
+		if(strcmp(nome, p->nome) == 0){
+			return p;
+			}
+	}
+	return NULL;
+}
+
+Movel *lista_movel_remove(Movel* lista, char nome[]){
+	Movel* anterior = NULL; 
+    Movel* p = lista;
+    while(strcmp(p->nome,nome)!=0){
+    	if (p==NULL){
+        	return lista;
+		}
+        anterior = p;
+        p = p->proximo;
+    }
+    if (anterior==NULL){
+        lista = p->proximo;
+	}	
+    else {
+        anterior->proximo = p->proximo;
+	}
+	printf("Movel removido com sucesso\n");
+	printf("Pressione enter para continuar...");
+	settings_press_enter();
+    free(p);
+    return lista;
+}
+
+Movel* lista_movel_edita(Movel* lista, char nome_busca[], char nome[], char tipo[], float preco, int qtd_estoque) { 
+	Movel *p;
+    for (p = lista; p != NULL; p = p->proximo) {
+        if (strcmp(nome_busca, p->nome) == 0) {
+            strcpy(p->nome, nome);
+            strcpy(p->tipo, tipo);
+            p->preco = preco;
+            p->qtd_estoque = qtd_estoque;
+			printf("Movel editado com sucesso\n");
+            return lista;
+        }
+    }
+    return lista;
+}
+
+int lista_movel_vazia(Movel* lista){ 
+	return (lista==NULL);
+}
+
+void lista_movel_imprime(Movel* lista){ 
+	Movel*p;
+	for(p = lista; p != NULL; p = p->proximo){
+		printf("Movel: %s\t\tPreco:R$%.1f\t\tTipo: %s\t\tQuantidade em estoque:%d\n", p->nome, p->preco,p->tipo, p->qtd_estoque);
+	}
+}
+ 
+void lista_movel_libera(Movel* lista){
+    Movel* p = lista;
+    Movel* t;
+    while (p != NULL) {
+        t = p->proximo;
+        free(p);
+        p = t;
+    }
+}
---
+++ project/src/settings.c
@@ -0,0 +1,56 @@
+#include "../includes/settings.h"
+
+void settings_menu(void){
+    
+    printf("\t\tCadastro de moveis/setores.\n");
+    printf("***************************\n");
+    printf("1-Adicionar Movel\n");
+    printf("2-Remover Movel\n");
+    printf("3-Editar Movel\n");
+    printf("4-Adicionar Setor\n");
+    printf("5-Remover Setor\n");
+    printf("6-Listar setores e moveis\n");
+    printf("7-Buscar Movel\n");
+    printf("8-Sair\n");
+    printf("***************************\n");
+}
+void settings_limpa_buffer(void){
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
+
+void settings_captalize(char *string){
+    int tamanho = strlen(string);
+    for (int i = 0; i < tamanho; i++){
+        string[i] = toupper(string[i]);
+    }
+}
+
+void settings_clear(void){
+    system("cls");
+}
+
+void settings_press_enter(void){
+    while(getchar() != '\n');
+}
+
+int settings_str_verify(char *string){
+    int verificador = strlen(string);
+    for (int i = 0; i < verificador; i++){
+        if ((string[i] >= 'A' && string [i] <= 'Z') || (string[i] >= 'a' && string[i] <= 'z') || string[i] == ' '){
+            continue;
+        }
+        else{
+            return 0;
+        }   
+    }
+    return 1;
+}
+
+int settings_options(char *opcao){
+    int tamanho = strlen(opcao);
+    if (tamanho > 1){
+        return 1;
+    }
+    return 0;
+}
